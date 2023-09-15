fa619ddd6087d6b0cdeeef15a6f41c566901eab3
Nabemh <nhanior@gmail.com>
2023-09-15 13:02:19 +0100

    _printf - trial 1

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    int char_out = 0;
+    va_list arg_set;
+    
+    if (!format)
+        return (-1);
+
+    va_start(arg_set, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            char_out++;
+        }
+        else 
+        {
+            format++;
+            if (*format == '\0')
+            break;
+            
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                char_out++;
+            }
+
+            else if (*format == 's')
+            {
+                char *s = va_arg(arg_set, char*);
+                int _strlen = 0;
+                
+                while (s[_strlen] != '\0')
+                {
+                    _strlen++;
+                }
+                write(1, s, _strlen);
+                char_out = _strlen + char_out;
+
+            }
+            else if(*format == 'c')
+            {
+                char c;
+                c = va_arg(arg_set, int);
+                write(1, &c, 1);
+                char_out++;
+            }
+        }
+
+        format++;
+    }
+
+    va_end(arg_set);
+    return (char_out);
+
+}
+int main()
+{
+    _printf("L\n");
+    _printf("%c\n", 'v');
+    _printf("%s\n", "strings");
+    _printf("%%\n");
+    return 0;
+
+}
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h> 
+#include <stdio.h>
+#include <stdarg.h>
 
+int _strlen(char *s);
 int _printf(const char *format, ...);
 
 #endif

bb655c8bcc0ebcdbdb88733953a9af393f7640dc
joe949 <mwangiharun949@gmail.com>
2023-09-16 09:34:52 -0700

    updated

--- function_3.c
+++ function_3.c
@@ -61,7 +61,7 @@ int _printptr(va_list type, char buff[], int flags,
 {
 	char add_c = 0, pt_add = ' ';
 	int ind = _BUFFSIZE - 2, length = 2, pt_add_start = 1;
-	unsigned long num adr;
+	unsigned long num_adr;
 	char outline[] = "0123456789abcdef";
 	void *adr = va_arg(type, void *);
 
@@ -71,7 +71,7 @@ int _printptr(va_list type, char buff[], int flags,
 	if (adr == NULL)
 		return (write(1, "(nil)", 5));
 
-	buff[_BUFFSIZE - 1] ='\0';
+	buff[_BUFFSIZE - 1] = '\0';
 	UNUSED(precision);
 
 	num_adr = (unsigned long)adr;
@@ -91,5 +91,84 @@ int _printptr(va_list type, char buff[], int flags,
 
 	ind++;
 	/*return (write(1, &buff[i], _BUFFSIZE -i -1));*/
-	return (write_ptr(buff, ind, length, width, flags, pt_add, add_c, pt_add_start));
+	return (write_ptr(buff, ind, length, width,
+		flags, pt_add, add_c, pt_add_start));
+}
+/*****PRINT POINTER****/
+/**
+ * _printrev - prints a string in reverse
+ * @type: a list of arguments
+ * @buff: buffer array that handles print
+ * @flags: get number of active flags
+ * @width: get width
+ * @precision: precision spec
+ * @size: size spec
+ * Return: number of characters printed
+ */
+int _printrev(va_list type, char buff[], int flags,
+	int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buff);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(type, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+	for (i = i - 1; i >= 0; i--)
+	{
+		char b = str[i];
+
+		write(1, &b, 1);
+		count++;
+	}
+	return (count);
+}
+/*****PRINT NON-PRINT CHARS****/
+/**
+ * _print_non_print - prnts ASCII codes in hexa and non-printable chars
+ * @type: a list of arguments
+ * @buff: buffer array that handles print
+ * @flags: get number of active flags
+ * @width: get width
+ * @precision: precision spec
+ * @size: size spec
+ * Return: number of characters printed
+ */
+int _print_non_print(va_list type, char buff[], int flags,
+	int width, int precision, int size)
+{
+	int i = 0, bal_out = 0;
+	char *str = va_arg(type, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[i] != '\0')
+	{
+		if (can_print(str[i]))
+			buff[i + bal_out] = str[i];
+		else
+			bal_out += hex_code(str[i], buff, i + bal_out);
+
+		i++;
+	}
+	buff[i + bal_out] = '\0';
+
+	return (write(1, buff, i + bal_out));
 }

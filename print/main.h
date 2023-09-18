4992e6d35658ef187282917e16e07fbb5ce51e8f
Nabemh <nhanior@gmail.com>
2023-09-18 10:44:11 +0100

    test folder created

--- print/main.h
+++ print/main.h
@@ -27,17 +27,31 @@ struct frmt
 typedef struct frmt frmt_t;
 
 int _printf(const char *format, ...);
-va_list (lst, char buff[]);
+int _printfnc(const char *fmt, int *i, va_list lst,
+	char buff[], int flags, int precision, int width, int size);
 /*functions*/
-int _printchar(va_list type, char buff[]);
-int _printstr(va_list type, char buff[]);
-int _printpcnt(va_list type, char buff[]);
-int _printint(va_list type, char buff[]);
-int _printbinary(va_list type, char buff[]);
-int _unsigned(va_list type, char buff[]);
-int _octal(va_list type, char buff[]);
-int _hexadecimal(va_list type, char buff[]);
-int _hexa_upper(va_list type, char buff[]);
-
-
+int _printchar(va_list type, char buff[], int flags,
+	int width, int precision, int size);
+int _printstr(va_list type, char buff[], int flags,
+	int width, int precision, int size);
+int _printpcnt(va_list type, char buff[], int flags,
+	int width, int precision, int size);
+int _printint(va_list type, char buff[], int flags,
+	int width, int precision, int size);
+int _printbinary(va_list type, char buff[], int flags
+	int width, int preecision, int size);
+int _printptr(va_list type, char buff[], int flag,
+	int width, int preecision, int size);
+int _print_non_print(va_list type, char buff[], int flags,
+	int width, int preecision, int size);
+int _printrev(va_list type, char buff[], int flags,
+	int width, int preecision, int size);
+int _unsigned(va_list type, char buffer[], int flags, 
+		int width, int precision, int size);
+int _octal(va_list type, char buffer[], int flags, 
+                int width, int precision, int size);
+int _hexadecimal(va_list type, char buffer[], int flags,
+                int width, int precision, int size);
+int _hexa_upper(va_list type, char buffer[], int flags,
+                int width, int precision, int size);
 #endif

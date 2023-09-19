b9b5bc03edba7462a5a4754b54890f0550ebf4b5
Nabemh <nhanior@gmail.com>
2023-09-19 07:53:20 +0100

    prototypes added

--- print/main.h
+++ print/main.h
@@ -59,4 +59,7 @@ int _hexadecimal(va_list type, char buffer[], int flags,
                 int width, int precision, int size);
 int _hexa_upper(va_list type, char buffer[], int flags,
                 int width, int precision, int size);
+int _precision(const char *format, int *arg_i, va_list arg_list);
+int _size(const char *format, int *arg_i);
+
 #endif

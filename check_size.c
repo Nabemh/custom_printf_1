4ab0f1f6fcbe0857ab3637d44839d80b106639bb
Nabemh <nhanior@gmail.com>
2023-09-19 22:25:09 +0100

    update

--- check_size.c
+++ check_size.c
@@ -11,17 +11,17 @@
 int _size(const char *format, int *arg_i)
 {
 	int init_val = (*arg_i + 1);
-	int str_size = 0;
+	int size = 0;
 
 	if (format[init_val] == 'l')
-		str_size = S_LONG;
+		size = S_LONG;
 	else if (format[init_val] == 'h')
-		str_size = S_SHORT;
+		size = S_SHORT;
 
-	if (str_size == 0)
+	if (size == 0)
 		*arg_i = init_val - 1;
 	else
 		*arg_i = init_val;
 
-	return (str_size);
+	return (size);
 }
